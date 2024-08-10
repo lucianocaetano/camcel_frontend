841afcdaa7abca09f52bebaff574d3761d962123
Mauro Caetano <lucianocaetano1999@gmail.com>
2024-08-10 10:28:34 -0300

    avances en crear empresa

--- src/pages/EmpresaDetalles.vue
+++ src/pages/EmpresaDetalles.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-      {{empresa}}
-  </div>
+    <div :if="!isLoading && !empresaNoExiste">
+        {{empresa}}
+    </div>
+    <div v-if="empresaNoExiste">
+        no existe esta empresa
+    </div>
+    <div v-if="isLoading" class="text-center">
+        loading... 
+    </div>
 </template>
 
 <script>
@@ -16,14 +22,19 @@ export default {
 
         const isLoading = ref(true)
         const empresa = ref(null)
+        const empresaNoExiste = ref(false)
 
         api.get(`enterprises/${params.slug}`).then((response) => {
             isLoading.value = false
             empresa.value = response.data
+        }).catch((err) => {
+            isLoading.value = false
+            if(err?.response?.status){
+                empresaNoExiste.value = true
+            }
         })
 
-        return {isLoading, empresa}
-
+        return {isLoading, empresa, empresaNoExiste}
     }
 }
 </script>
--- src/pages/Empresas.vue
+++ src/pages/Empresas.vue
@@ -14,9 +14,38 @@
       </q-input>
 
       <q-toolbar class="row reverse">
-         <q-btn flat bordered>
+         <q-btn flat bordered @click="enterpriseCreate = true">
             <q-avatar icon="mdi-plus-circle-outline" />
          </q-btn>
+         <q-dialog v-model="enterpriseCreate" >
+           <q-card style="width: 900px;">
+             <q-card-section>
+               <div class="text-h6">Crear Empresa</div>
+             </q-card-section>
+
+             <q-card-section class="q-pt-none">
+                <q-form @submit.prevent="handleCreateEnteprise">
+                  <q-input name="RUT" label="RUT" v-model="dataCreateEnteprise.RUT"/>
+                  <q-input name="nombre" label="nombre" v-model="dataCreateEnteprise.nombre"/>
+                  <q-checkbox v-model="dataCreateEnteprise.valid" label="Verificado" />
+
+                  <q-file color="teal" filled label="image" v-model="dataCreateEnteprise.image">
+                    <template v-slot:prepend>
+                      <q-icon name="cloud_upload" />
+                    </template>
+                  </q-file>
+
+
+                  <q-btn label="Crear" class="q-mt-md" type="submit" color="primary"/>
+                </q-form>
+             </q-card-section>
+
+             <q-card-actions align="right">
+               <q-btn flat label="Cerrar" color="primary" v-close-popup />
+             </q-card-actions>
+           </q-card>
+         </q-dialog>
+
       </q-toolbar>
       
    </q-toolbar>
@@ -32,22 +61,31 @@
 <script>
 import BotonEmpresas from 'src/components/BotonEmpresas.vue';
 import { api } from "src/boot/axios";
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
 export default {
    components: {
       BotonEmpresas
    }, setup () {
       const search = ref("");
+      const enterpriseCreate = ref(false);
       const isLoading = ref(true)
       const empresas = ref(null)
+
+      const dataCreateEnteprise = reactive({
+         nombre: "", RUT: "", valid: false, image: null
+      })
 
       api.get("enterprises").then((response) => {
          isLoading.value = false
          empresas.value = response.data
       })
 
-      return {isLoading, empresas, search}
+      const handleCreateEnteprise = () => {
+         console.log(dataCreateEnteprise.image)
+      }
+
+      return {isLoading, empresas, search, enterpriseCreate, handleCreateEnteprise, dataCreateEnteprise}
    }
 }
 

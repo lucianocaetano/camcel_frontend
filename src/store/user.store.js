35a75481dc55d695d67c8f87012c6f610f9d7210
Juan Suarez <suarez.04112006@gmail.com>
2024-10-07 11:46:10 -0300

    se soluciona el cierre de sesion

--- src/store/user.store.js
+++ src/store/user.store.js
@@ -1,10 +1,9 @@
 import { defineStore } from 'pinia';
 
-
 export const useUserStore = defineStore('user', {
   state: () => ({
     token: localStorage.getItem("token"),
-    auth: localStorage.getItem("auth")? true: false,
+    auth: localStorage.getItem("auth"),
     user: localStorage.getItem("user"),
     users: [] // Aquí agregamos la lista de usuarios
   }),
@@ -28,18 +27,30 @@ export const useUserStore = defineStore('user', {
       this.token = token;
     },
     setAuth(auth) {
-      localStorage.setItem("auth", auth)
-      if(!auth){
-        localStorage.removeItem("token")
-        localStorage.removeItem("user")
-        this.token = null
-        this.user = []
+      localStorage.setItem("auth", auth);
+      if (!auth) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        this.token = null;
+        this.user = null;
       }
       this.auth = auth;
     },
     setUser(user) {
-      localStorage.setItem("user", JSON.stringify(user))
+      localStorage.setItem("user", user);
       this.user = user;
     },
+    setUsers(users) {
+      this.users = users; // Nueva acción para establecer la lista de usuarios
+    },
+    clearSession() {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      this.token = null;
+      this.user = null;
+    },
+    logout() {
+      this.setAuth(false); // Esto también llamará a clearSession() si auth es falso
+    }  
   },
 });

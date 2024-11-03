3c8ec33af081e40940af85bcba248fcfa2be611f
Juan Suarez <suarez.04112006@gmail.com>
2024-11-03 13:00:48 -0300

    conectamos trabajo con la BdD

--- src/pages/Trabajo.vue
+++ src/pages/Trabajo.vue
@@ -1,36 +1,28 @@
-<template >
-
-
-  <q-page >
+<template>
+  <q-page>
     <div class="ola q-mx-auto">
-    
-    <q-list bordered>
-      <q-item bordered>
-        <q-item-section class="col-2"> 
-          <q-item-label header>Nombre Empresa</q-item-label>
-        </q-item-section>
-        <q-item-section class="col-2"> 
-          <q-item-label header>Trabajo a realizar</q-item-label>
-        </q-item-section>
-        <q-item-section class="col-2 text-center"> 
-          <q-item-label header>fecha <br>
-                              * Entrada <br>
-                              * Salida</q-item-label>
-        </q-item-section>
-       
-        <q-item-section class="col-2 text-center"> 
-          <q-item-label header>Confirmación <br>
-                               Prevencionista </q-item-label>
-        </q-item-section>
-        <q-item-section class="col-2 text-center"> 
-          <q-item-label header>Confirmación <br>
-                               Empresa </q-item-label>
-        </q-item-section>
-        <q-item-section class="col-2 text-center"> 
-          <q-item-label header>Mas informacion</q-item-label>
-        </q-item-section>
-      </q-item>
       <q-list bordered>
+        <q-item bordered>
+          <q-item-section class="col-2">
+            <q-item-label header>Nombre Empresa</q-item-label>
+          </q-item-section>
+          <q-item-section class="col-2">
+            <q-item-label header>Trabajo a realizar</q-item-label>
+          </q-item-section>
+          <q-item-section class="col-2 text-center">
+            <q-item-label header>Fecha <br>* Entrada <br>* Salida</q-item-label>
+          </q-item-section>
+          <q-item-section class="col-2 text-center">
+            <q-item-label header>Confirmación <br>Prevencionista</q-item-label>
+          </q-item-section>
+          <q-item-section class="col-2 text-center">
+            <q-item-label header>Confirmación <br>Empresa</q-item-label>
+          </q-item-section>
+          <q-item-section class="col-2 text-center">
+            <q-item-label header>Más información</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-list bordered>
         <q-item >
           <q-item-section class="col-12 text-center" > 
             <q-btn  color="green" @click="dialogVisible = true" class="col-12">Registrar trabajo</q-btn>
@@ -120,113 +112,63 @@
     </q-card-actions>
   </q-card>
 </q-dialog>
-      <q-item v-for="(actividad, index) in actividadesFiltradas" :key="index" :class="{'bg-grey-4': index % 2 === 0}">
-        <q-item-section class="col-2"> 
-          {{ actividad.nombre }} 
-        </q-item-section>
-        <q-item-section class="col-2"> 
-          {{ actividad.trabajo }}
-        </q-item-section>
-        <q-item-section class="col-2 text-center"> 
-          {{ actividad.fechaInicio }} <br>
-          {{ actividad.fechaFin }}
-        </q-item-section>
-      
-        <q-item-section class="col-3 text-center">
-          <div>
-            <q-btn round v-if="actividad.confirmacionPREV === null" color="green" @click="confirmarPREV(index)" icon="mdi-check-circle" />
-            <q-btn round v-if="actividad.confirmacionPREV === null" color="red"  @click="denegarPREV(index)" icon="mdi-close-circle" />
-            
-            <q-icon v-if="actividad.confirmacionPREV === true" name="mdi-check-circle" color="green" size="40px" style="padding-right: 20px;"> </q-icon>
-            <q-icon v-if="actividad.confirmacionPREV === false" name="mdi-close-circle" color="red" size="40px" style="padding-right: 20px;"> </q-icon>
-            
-            
-          </div>
-          <q-item-section class="col-1 text-center">
 
+        <q-item v-for="(actividad, index) in actividadesFiltradas" :key="index" :class="{'bg-grey-4': index % 2 === 0}">
+          <q-item-section class="col-2">
+            {{ actividad.nombre }}
           </q-item-section>
-          </q-item-section>
-          <q-item-section class="col-1 text-center">
-          
-            
-            <q-icon v-if="actividad.confirmacionEmpresa" name="mdi-check-circle" color="green" size="40px" > </q-icon>
-            <q-icon v-if="!actividad.confirmacionEmpresa" name="mdi-close-circle" color="red" size="40px"> </q-icon>
-          
-        </q-item-section>
-        <q-item-section class="col-2 text-center">
-          <q-btn class="q-mx-auto" style="background-color: white;"> <q-icon name="description" size="30px"></q-icon></q-btn>
-        </q-item-section>
-      </q-item> 
-    </q-list> 
-   </div>
-  </q-page>
-  <!--
-  <q-page v-if="isMobile()">
-    <div class="ola q-mx-auto">
-      <q-list bordered>
-        <q-item bordered>
-          <q-item-section class="col-4">
-            <q-item-label header>Trabajo a realizar</q-item-label>
-          </q-item-section>
-          <q-item-section class="col-4">
-            <q-item-label header>Fecha Inicio</q-item-label>
-          </q-item-section>
-          <q-item-section class="col-4">
-            <q-item-label header>Fecha Fin</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item v-for="(actividad, index) in actividadesFiltradas" :key="index" :class="{'bg-grey-4': index % 2 === 0}" @click="abrirDetalles(actividad)">
-          <q-item-section class="col-4">
+          <q-item-section class="col-2">
             {{ actividad.trabajo }}
           </q-item-section>
-          <q-item-section class="col-4">
-            {{ actividad.fechaInicio }}
-          </q-item-section>
-          <q-item-section class="col-4">
+          <q-item-section class="col-2 text-center">
+            {{ actividad.fechaInicio }} <br>
             {{ actividad.fechaFin }}
+          </q-item-section>
+          <q-item-section class="col-3 text-center">
+            <div>
+              <q-btn round v-if="actividad.confirmacionPREV === null" color="green" @click="confirmarPREV(index)" icon="mdi-check-circle" />
+              <q-btn round v-if="actividad.confirmacionPREV === null" color="red" @click="denegarPREV(index)" icon="mdi-close-circle" />
+              <q-icon v-if="actividad.confirmacionPREV === true" name="mdi-check-circle" color="green" size="40px" style="padding-right: 20px;"></q-icon>
+              <q-icon v-if="actividad.confirmacionPREV === false" name="mdi-close-circle" color="red" size="40px" style="padding-right: 20px;"></q-icon>
+            </div>
+          </q-item-section>
+          <q-item-section class="col-1 text-center">
+            <q-icon v-if="actividad.confirmacionEmpresa" name="mdi-check-circle" color="green" size="40px"></q-icon>
+            <q-icon v-if="!actividad.confirmacionEmpresa" name="mdi-close-circle" color="red" size="40px"></q-icon>
+          </q-item-section>
+          <q-item-section class="col-2 text-center">
+            <q-btn class="q-mx-auto" style="background-color: white;">
+              <q-icon name="description" size="30px"></q-icon>
+            </q-btn>
           </q-item-section>
         </q-item>
       </q-list>
-      
-     
-      <q-btn color="green" @click="registrar" class="q-mx-auto">Registrar trabajo</q-btn>
-
-      
-      <q-dialog v-model="dialogVisible">
-        <q-card>
-          <q-card-section>
-            <div>
-              <h4>{{ actividadSeleccionada.trabajo }}</h4>
-              <p>Nombre Empresa: {{ actividadSeleccionada.nombre }}</p>
-              <p>Hora Entrada: {{ actividadSeleccionada.horaEntrada }}</p>
-              <p>Hora Salida: {{ actividadSeleccionada.horaSalida }}</p>
-              <p>Fecha Inicio: {{ actividadSeleccionada.fechaInicio }}</p>
-              <p>Fecha Fin: {{ actividadSeleccionada.fechaFin }}</p>
-              <p>Confirmación Prevencionista: {{ actividadSeleccionada.confirmacionPREV }}</p>
-              <p>Confirmación Empresa: {{ actividadSeleccionada.confirmacionEmpresa }}</p>
-            </div>
-          </q-card-section>
-          <q-card-actions>
-            <q-btn label="Cerrar" @click="dialogVisible = false" />
-          </q-card-actions>
-        </q-card>
-      </q-dialog>
     </div>
   </q-page>
-  -->
 </template>
-
-
- 
-
 
 <script setup>
 import { ref, computed, onMounted } from 'vue';
 import axios from 'axios';
-const timeE = ref("")
-const timeS = ref ("")
-const step = ref(0)
+import { useEnterpriseStore } from "src/store/enterprise.store"
+import { api } from "src/boot/axios";
+
+const enterpriseStore = useEnterpriseStore()
+const actividades = ref([]);
+const dialogVisible = ref(false);
+const timeE = ref("");
+const timeS = ref("");
+const step = ref(0);
+const newActividad = ref({
+  nombre: '',
+  trabajo: '',
+  fechaInicio: '',
+  fechaFin: '',
+  horaEntrada: '',
+  horaSalida: '',
+  confirmacionEmpresa: false,
+  confirmacionPREV: null,
+});
 
 function stepmas() {
   if (step.value < 2) {
@@ -239,121 +181,44 @@ function stepmenos() {
   }
 }
 
-
-const dialogVisible = ref(false);
-const actividades = ref([
-  { nombre: "Los Pinos", trabajo: "Revisar sistema eléctrico", horaEntrada: "8:00", horaSalida: "17:00", fechaInicio: "2024-10-11", fechaFin: "2024-10-24", completada: false, confirmacionPREV: true, confirmacionEmpresa: true },
-  { nombre: "El Roble", trabajo: "Reparar sistema de agua", horaEntrada: "9:00", horaSalida: "18:00", fechaInicio: "2024-10-12", fechaFin: "2024-10-25", completada: false, confirmacionPREV: false, confirmacionEmpresa: false },
-  { nombre: "Industria Metalúrgica", trabajo: "Mantenimiento de máquinas", horaEntrada: "7:30", horaSalida: "16:00", fechaInicio: "2024-08-25", completada: false, confirmacionPREV: false, confirmacionEmpresa: false },
-  { nombre: "Industria Metalúrgica", trabajo: "Mantenimiento de máquinas", horaEntrada: "7:30", horaSalida: "16:00", fechaInicio: "2024-10-13", fechaFin: "2024-10-26", completada: false, confirmacionPREV: false, confirmacionEmpresa: true },
-  { nombre: "Colegio San Martín", trabajo: "Instalación de paneles solares", horaEntrada: "7:00", horaSalida: "15:00", fechaInicio: "2024-10-14", fechaFin: "2024-10-27", completada: false, confirmacionPREV: true, confirmacionEmpresa: false },
-  { nombre: "Hospital Central", trabajo: "Reparación de generador", horaEntrada: "6:30", horaSalida: "14:30", fechaInicio: "2024-10-15", fechaFin: "2024-10-28", completada: false, confirmacionPREV: null, confirmacionEmpresa: false },
-  { nombre: "Taller Mecánico López", trabajo: "Reemplazo de luces fluorescentes", horaEntrada: "10:00", horaSalida: "19:00", fechaInicio: "2024-10-16", fechaFin: "2024-10-29", completada: false, confirmacionPREV: null, confirmacionEmpresa: false },
-  { nombre: "Fábrica de Plásticos", trabajo: "Inspección de seguridad", horaEntrada: "8:30", horaSalida: "17:30", fechaInicio: "2024-10-17", fechaFin: "2024-10-30", completada: false, confirmacionPREV: null, confirmacionEmpresa: false },
-  { nombre: "Universidad Nacional", trabajo: "Revisión de red eléctrica", horaEntrada: "7:45", horaSalida: "16:15", fechaInicio: "2024-10-18", fechaFin: "2024-10-31", completada: false, confirmacionPREV: null, confirmacionEmpresa: false },
-  { nombre: "Estación de Bomberos", trabajo: "Mantenimiento de equipo", horaEntrada: "7:00", horaSalida: "15:30", fechaInicio: "2024-10-19", fechaFin: "2024-11-01", completada: false, confirmacionPREV: null, confirmacionEmpresa: false },
-  { nombre: "Centro Comercial Las Flores", trabajo: "Instalación de cámaras de seguridad", horaEntrada: "9:00", horaSalida: "18:00", fechaInicio: "2024-10-20", fechaFin: "2024-11-02", completada: false, confirmacionPREV: null, confirmacionEmpresa: false }
-]);
-
+// Computed para filtrar las actividades
 const actividadesFiltradas = computed(() => {
-  const hoy = new Date().toISOString().split('T')[0]; 
+  const hoy = new Date().toISOString().split('T')[0];
   return actividades.value.filter(actividad => actividad.fechaInicio >= hoy);
 });
 
-function confirmarHoraEntrada(index) {
-  actividades.value[index].entradaConfirmada = true;
-}
-const newActividad = ref({
-  nombre: '',
-  trabajo: '',
-  fechaInicio: '',
-  fechaFin: '',
-  horaEntrada: '',
-  horaSalida: '',
-  solicitarCI: false,
-  solicitarBPS: false,
-  solicitarBSE: false,
-  solicitarInduccionSeguridad: false,
-  permisoTrabajoEnCaliente: false,
-  permisoTrabajoEnAltura: false,
-  permisoTrabajoEnEspacioConfinado: false,
-  permisoTrabajoOtros: false,
+// Función para obtener actividades desde la API
+
+api.get("admin/jobs").then((response) => {
+  console.log(response.data);
+  
+  actividades.value = response.data.jobs.map(job => ({
+    nombre: job.enterprise_id, // Reemplaza según la estructura de tu respuesta
+    trabajo: job.title,
+    fechas: job.fechas || [], // Ajusta según el nuevo campo de fechas
+    horaEntrada: job.in_time,
+    horaSalida: job.out_time,
+    completada: false, // O ajusta según lo necesario
+    confirmacionPREV: null, // O ajusta según lo necesario
+    confirmacionEmpresa: job.is_check_enterprise,
+  }));
+  
+  return actividades.value;
 });
 
-function registrarActividad() {
-  // Verificar que los campos obligatorios estén completos
-  if (newActividad.value.nombre && newActividad.value.trabajo && newActividad.value.fechaInicio && newActividad.value.fechaFin) {
-    // Agregar la nueva actividad al ref actividades
-    actividades.value.push({
-      nombre: newActividad.value.nombre,
-      trabajo: newActividad.value.trabajo,
-      fechaInicio: newActividad.value.fechaInicio,
-      fechaFin: newActividad.value.fechaFin,
-      horaEntrada: timeE,
-      horaSalida: timeS,
-      completada: false, // Puedes establecer el estado completada según sea necesario
-      confirmacionPREV: null, // Otras propiedades inicializadas si es necesario
-      confirmacionEmpresa: newActividad.value.confirmacionEmpresa,
-      solicitarCI: newActividad.value.solicitarCI,
-      solicitarBPS: newActividad.value.solicitarBPS,
-      solicitarBSE: newActividad.value.solicitarBSE,
-      solicitarInduccionSeguridad: newActividad.value.solicitarInduccionSeguridad,
-      permisoTrabajoEnCaliente: newActividad.value.permisoTrabajoEnCaliente,
-      permisoTrabajoEnAltura: newActividad.value.permisoTrabajoEnAltura,
-      permisoTrabajoEnEspacioConfinado: newActividad.value.permisoTrabajoEnEspacioConfinado,
-      permisoTrabajoOtros: newActividad.value.permisoTrabajoOtros,
-    });
-
-    // Cerrar el diálogo o formulario
-    dialogVisible.value = false;
-
-    // Limpiar los campos del formulario
-    newActividad.value = {
-      nombre: '',
-      trabajo: '',
-      fechaInicio: '',
-      fechaFin: '',
-      solicitarCI: false,
-      solicitarBPS: false,
-      solicitarBSE: false,
-      solicitarInduccionSeguridad: false,
-      permisoTrabajoEnCaliente: false,
-      permisoTrabajoEnAltura: false,
-      permisoTrabajoEnEspacioConfinado: false,
-      permisoTrabajoOtros: false,
-    };
-  } else {
-    alert('Por favor, complete todos los campos');
-  }
-}
+ 
+   
 
 
-const borrarActividadesFiltradas = () =>{
-  const hoy = new Date().toISOString().split('T')[0];
-  actividades.value = actividades.value.filter(actividad => actividad.fechaInicio >= hoy);
-}
-
-
-function confirmarPREV(index) {
-  actividades.value[index].confirmacionPREV = !actividades.value[index].confirmacionPREV;
-}
-function denegarPREV(index) {
-  actividades.value[index].confirmacionPREV = false;
-}
-
-
-onMounted(()=>{borrarActividadesFiltradas();});
-
+// Llama a la función en el mounted
 
 </script>
+
 <style scoped>
-
-
 @media only screen and (max-width: 1022px) {
-.ola{
-  overflow-x: scroll;
-  width: 900px;
+  .ola {
+    overflow-x: scroll;
+    width: 900px;
+  }
 }
-}
-  
 </style>

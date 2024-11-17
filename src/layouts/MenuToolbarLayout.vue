3a5c1ddb509117d52ae6973659964dd1fe065080
Juan Suárez <suarez.04112006@gmail.com>
2024-11-17 19:14:07 -0300

    Implementando ultimos cambios en esta rama

--- src/layouts/MenuToolbarLayout.vue
+++ src/layouts/MenuToolbarLayout.vue
@@ -1,44 +1,35 @@
 <template>
   <div class="q-pa-md">
-    <q-header
-      elevated
-      style="padding: 0 20px"
-      class="row justify-between bg-teal-10"
-    >
+    <q-header elevated style="padding: 0 20px;" class="row justify-between bg-teal-10">
       <div>
         <q-toolbar class="row items-center">
           <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
           <q-toolbar-title>Camcel</q-toolbar-title>
         </q-toolbar>
       </div>
+     
       <div class="row">
-        <Notificaciones2 />
-        <chat />
+        <Notificaciones2/>
+        <chat/>
         <q-btn flat icon="person">
           <q-menu>
-            <q-list style="min-width: 100px">
-              <q-item clickable v-close-popup>
-                <q-item-section>Perfil</q-item-section>
-              </q-item>
+            <q-list>
 
-              <q-separator />
-              <q-item clickable v-close-popup>
-                <q-item-section>Empresa</q-item-section>
-              </q-item>
               <q-separator />
               <q-item clickable v-close-popup to="/Configuracion">
                 <q-item-section>Configuracion</q-item-section>
               </q-item>
+
               <q-separator />
               <q-item clickable @click="handleLogout" v-close-popup>
-                <q-item-section>Cerrar Sesion</q-item-section>
+                <q-item-section >Cerrar Sesion</q-item-section>
               </q-item>
             </q-list>
           </q-menu>
         </q-btn>
       </div>
     </q-header>
-
+    
     <q-drawer
       v-model="drawer"
       show-if-above
@@ -47,86 +38,86 @@
       bordered
       :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
     >
-      <q-scroll-area class="fit">
-        <q-list>
-          <template v-for="(menuItem, index) in menuList" :key="index">
-            <q-item
-              clickable
-              v-ripple
-              class="text-black"
-              :to="{ name: menuItem.href }"
-            >
-              <q-item-section avatar>
-                <q-icon :name="menuItem.icon" />
-              </q-item-section>
-              <q-item-section>
-                {{ menuItem.label }}
-              </q-item-section>
-            </q-item>
-            <q-separator :key="'sep' + index" v-if="menuItem.separator" />
-          </template>
+    <q-scroll-area
+        
+        style="
+          height: calc(100% - 150px);
+          
+          
+        "
+      >
+        <q-list padding>
+          <q-item clickable v-ripple v-for="(boton , index) in botones"
+          :key="index"  @click="pagina(boton.pagina)">
+            <q-item-section  avatar>
+              <q-icon  :name="boton.icono" />
+            </q-item-section>
+
+            <q-item-section > {{ boton.nombre }} </q-item-section>
+          </q-item>
         </q-list>
+          
       </q-scroll-area>
+
+      
     </q-drawer>
+
     <q-page-container>
-      <q-page class="q-mx-auto" style="max-width: 2000px">
-        <slot />
+      <q-page class="q-mx-auto" style="max-width: 2000px;">
+        <slot/>
       </q-page>
     </q-page-container>
   </div>
 </template>
 
-<script>
-import { ref } from "vue";
+<script setup>
+import { ref } from 'vue';
+import Chat from 'src/components/Chat.vue'
+import notificaciones from 'src/components/Notificaciones.vue' 
+import { useRouter } from "vue-router"
+import { useUserStore } from 'src/store/user.store.js'
+import Notificaciones2 from 'src/components/Notificaciones2.vue';
 
-export default {
-  setup() {
-    const search = ref("");
+const router = useRouter()
+const userStore = useUserStore()
+const handleLogout = () => {
+  userStore.logout() // Llama al método logout del store para limpiar el estado y el localStorage
+  
+  // Redirige al usuario a la página de inicio de sesión
+  router.push('/login')
+}
+// Definición de estados reactivos
+const text = ref('');
+const dialog = ref(false);
+const maximizedToggle = ref(true);
 
-    const menuList = [
-      {
-        icon: "business_center",
-        label: "Empresas",
-        href: "enterprises",
-        separator: false,
-      },
-      {
-        icon: "mdi-account",
-        label: "Usuarios",
-        href: "users",
-        separator: false,
-      },
-      {
-        label: "His. trabajo",
-        icon: "mdi-folder-multiple",
-        href: "his.trabajo",
-      },
-      { label: "trabajos", icon: "mdi-account-hard-hat", href: "trabajos" },
-      {
-        label: "Soporte",
-        icon: "mdi-cog-outline",
-        href: "soporte",
-        separator: true,
-      },
-
-      {
-        icon: "settings",
-        label: "Settings",
-        separator: false,
-      },
-      {
-        icon: "logout",
-        iconColor: "primary",
-        label: "Logout",
-        separator: false,
-      },
-    ];
-
-    return {
-      drawer: ref(false),
-      search,
-      menuList,
-    };
+const search = ref('');
+const botones = ref([
+    {nombre:"Empresas",icono:"mdi-office-building-cog-outline",pagina:"/" },
+    {nombre:"Usuarios",icono:"mdi-account-multiple-outline",pagina:"/usuarios" },
+    {nombre:"His. trabajo",icono:"mdi-folder-multiple",pagina:"/his.trabajo" },
+    {nombre:"Trabajos",icono:"mdi-account-hard-hat",pagina:"/trabajos" },
+    {nombre:"Soporte",icono:"mdi-cog-outline",pagina:"/soporte" },
+    
+    
+  ])
+  const pagina =(e)=>{
+    router.push(e)
+  }
+const menuList = [
+  {
+    icon: 'settings',
+    label: 'Settings',
+    separator: false
   },
-};
+  {
+    icon: 'logout',
+    iconColor: 'primary',
+    label: 'Logout',
+    separator: false
+  }
+];
+
+// Definición de `drawer`
+const drawer = ref(false);
 </script>
